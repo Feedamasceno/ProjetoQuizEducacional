5c609c699d285aedb07c75c8700fa9c1b3b773d2
Felipe Damasceno <felipe.damasceno.b@gmail.com>
2022-10-02 13:48:22 -0300

    added first file

---
+++ Projeto - Quiz Educacional.py
@@ -0,0 +1,105 @@
+print('Bem vindo ao quiz!')
+
+pergunta1 = input(
+  "1) Assinale a alternativa que apresenta o significado da Educação Ambiental.: \n a) Compreende os processos por meio dos quais o indivíduo e a coletividade constroem valores sociais, conhecimentos, habilidades, atitudes e competências voltadas para a conservação do meio ambiente, que favorecem a qualidade de vida e a sustentabilidade. \n b) Compreende os processos por meio do qual um indivíduo constrói valores sociais, conhecimentos, atitudes e competências voltadas para a conservação do meio ambiente, essencial somente para a sustentabilidade \n c) A educação ambiental é um componente essencial que deve estar presente, somente, no processo educativo das crianças. \n d) A concepção da educação ambiental em sua totalidade, visa a vinculação entre a ética, a educação, o trabalho e as práticas sociais para os estudos de um meio ambiente menos sustentável. \n e) . \n R: "
+)
+
+cont = 0
+
+if pergunta1 == "A" or "a":
+  print("Correto!")
+  cont = cont + 1
+else:
+  print(f"A resposta é 'A', não {pergunta1!r}")
+
+pergunta2 = input(
+  "2) Qual o Estado que mais produz lixo diariamente no Brasil?: \n a) Rio de Janeiro. \n b) São Paulo. \n c) Minas Gerais. \n d) Ceará. \n e) Bahia. \n R: "
+)
+
+if pergunta2 == "B" or "b":
+  print("Correto!")
+  cont = cont + 1
+else:
+  print(f"A resposta é 'B', não {pergunta2!r}")
+
+pergunta3 = input(
+  "3) A pontuação numa prova de 25 questões é a seguinte: +4 por questão respondida corretamente e –1 por questão respondida de forma errada. Para que um aluno receba nota correspondente a um número positivo, deverá acertar no míni: \n a) 3 questões \n b) 4 questões \n c) 5 questões \n d) 6 questões \n e) 7 questões \n R: "
+)
+
+if pergunta3 == ("a"):
+  print("Correto!")
+  cont = cont + 1
+else:
+  print(f"A resposta é 'A', não {pergunta3!r}")
+
+pergunta4 = input(
+  "4) A pontuação numa prova de 25 questões é a seguinte: +4 por questão respondida corretamente e –1 por questão respondida de forma errada. Para que um aluno receba nota correspondente a um número positivo, deverá acertar no míni: \n a) 3 questões \n b) 4 questões \n c) 5 questões \n d) 6 questões \n e) 7 questões \n R: "
+)
+
+if pergunta4 == ("a"):
+  print("Correto!")
+  cont = cont + 1
+else:
+  print(f"A resposta é 'A', não {pergunta4!r}")
+
+pergunta5 = input(
+  "5) A pontuação numa prova de 25 questões é a seguinte: +4 por questão respondida corretamente e –1 por questão respondida de forma errada. Para que um aluno receba nota correspondente a um número positivo, deverá acertar no míni: \n a) 3 questões \n b) 4 questões \n c) 5 questões \n d) 6 questões \n e) 7 questões \n R: "
+)
+
+if pergunta5 == ("a"):
+  print("Correto!")
+  cont = cont + 1
+else:
+  print(f"A resposta é 'A', não {pergunta5!r}")
+
+pergunta6 = input(
+  "6) A pontuação numa prova de 25 questões é a seguinte: +4 por questão respondida corretamente e –1 por questão respondida de forma errada. Para que um aluno receba nota correspondente a um número positivo, deverá acertar no míni: \n a) 3 questões \n b) 4 questões \n c) 5 questões \n d) 6 questões \n e) 7 questões \n R: "
+)
+
+if pergunta6 == ("a"):
+  print("Correto!")
+  cont = cont + 1
+else:
+  print(f"A resposta é 'A', não {pergunta6!r}")
+
+pergunta7 = input(
+  "7) A pontuação numa prova de 25 questões é a seguinte: +4 por questão respondida corretamente e –1 por questão respondida de forma errada. Para que um aluno receba nota correspondente a um número positivo, deverá acertar no míni: \n a) 3 questões \n b) 4 questões \n c) 5 questões \n d) 6 questões \n e) 7 questões \n R:"
+)
+
+if pergunta7 == ("a"):
+  print("Correto!")
+  cont = cont + 1
+else:
+  print(f"A resposta é 'A', não {pergunta7!r}")
+
+pergunta8 = input(
+  "8) A pontuação numa prova de 25 questões é a seguinte: +4 por questão respondida corretamente e –1 por questão respondida de forma errada. Para que um aluno receba nota correspondente a um número positivo, deverá acertar no míni: \n a) 3 questões \n b) 4 questões \n c) 5 questões \n d) 6 questões \n e) 7 questões \n R: "
+)
+
+if pergunta8 == ("a"):
+  print("Correto!")
+  cont = cont + 1
+else:
+  print(f"A resposta é 'A', não {pergunta8!r}")
+
+pergunta9 = input(
+  "9) A pontuação numa prova de 25 questões é a seguinte: +4 por questão respondida corretamente e –1 por questão respondida de forma errada. Para que um aluno receba nota correspondente a um número positivo, deverá acertar no míni: \n a) 3 questões \n b) 4 questões \n c) 5 questões \n d) 6 questões \n e) 7 questões \n R: "
+)
+
+if pergunta9 == ("a"):
+  print("Correto!")
+  cont = cont + 1
+else:
+  print(f"A resposta é 'A', não {pergunta9!r}")
+
+pergunta10 = input(
+  "10) A Coleta Seletiva é um mecanismo de recolha dos resíduos, os quais são classificados de acordo com sua origem e depositados em contentores indicados por cores. Marque a alternativa que corresponde corretamente o material e a cor de seu contentor: \n a) Azul: papéis e papelões; Verde: vidros; Vermelho: plásticos; Amarelo: metais; Marrom: resíduos orgânicos; \n b) Azul: plásticos; Verde: metais; Vermelho: papéis e papelões; Amarelo: vidros; Marrom: resíduos orgânicos; \n Azul: papéis e papelões; Verde: resíduos orgânicos; Vermelho: plásticos; Amarelo: metais; Marrom: vidros;  \n d) Azul: papéis e papelões; Verde: vidros; Vermelho: plásticos; Amarelo: metais; Marrom: resíduos orgânicos;  \n e) Azul: vidros; Verde: papéis e papelões; Vermelho: resíduos orgânicos; Amarelo: plásticos; Marrom: metais;   \n R: "
+)
+
+if pergunta10 == ("a"):
+  print("Correto!")
+  cont = cont + 1
+else:
+  print(f"A resposta é 'A', não {pergunta10!r}")
+
+print('A sua pontuação final foi:', (cont))
